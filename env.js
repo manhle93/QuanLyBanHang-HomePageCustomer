ff091625ac4164ea4998e816a98acdebac28faa8
ManhLe <manhle93@gmail.com>
2020-08-28 16:01:42 +0700

    fix

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
-// export const END_POINT = 'http://apiruongbacthang.skymapglobal.vn/api/'
-// export const END_POINT_IMAGE = 'http://apiruongbacthang.skymapglobal.vn/'
+export const END_POINT = 'http://apiruongbacthang.skymapglobal.vn/api/'
+export const END_POINT_IMAGE = 'http://apiruongbacthang.skymapglobal.vn/'
 
 
-export const END_POINT = 'http://127.0.0.1:8000/api/'
-export const END_POINT_IMAGE = 'http://127.0.0.1:8000/'
+// export const END_POINT = 'http://127.0.0.1:8000/api/'
+// export const END_POINT_IMAGE = 'http://127.0.0.1:8000/'
