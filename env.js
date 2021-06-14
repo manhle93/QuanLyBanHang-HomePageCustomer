0965ac0feebdbaa9649b801af09ad51aa628bf3f
ManhLe <zerokisss@gmail.com>
2021-06-14 15:48:02 +0700

    change endpoin

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
-export const END_POINT = 'http://apiruongbacthang.skymapglobal.vn/api/'
-export const END_POINT_IMAGE = 'http://apiruongbacthang.skymapglobal.vn/'
+export const END_POINT = 'http://apiruongbacthang.ruongbacthang.com.vn/api/'
+export const END_POINT_IMAGE = 'http://apiruongbacthang.ruongbacthang.com.vn/'
 
 
 // export const END_POINT = 'http://127.0.0.1:8000/api/'
